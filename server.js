c171e9cdcb5eca563b7f7cf1d1d42e626796d040
William White <willcwhite@gmail.com>
2019-08-30 12:06:50 -0400

    adds sequelize models, syncs with database

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const app = express()
+const app = express();
 const PORT = process.env.PORT || 8080;
 const db = require("./models")
 
@@ -11,9 +11,9 @@ app.use(express.static("./public"));
 require("./routes/html-routes.js")(app);
 //require("./routes/api-routes.js")(app);
 
- 
-//db.sequelize.sync().then(function() {
+ //syncs models with database, will drop current database and recreate if exists
+db.sequelize.sync({force:true}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
-//});
+});
