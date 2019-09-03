a840ff151553cdabe3becc47d7c234c42fbc4f92
Eric Billings <billings.company@gmail.com>
2019-09-03 14:27:17 -0400

    sending tattoo to DB

--- public/js/fontAPI.js
+++ public/js/fontAPI.js
@@ -1,4 +1,4 @@
-const families = ['Tangerine', 'Akronim', 'Luckiest Guy', 'Lobster', 'Wire One', 'Allura', 'Amatic SC', 'Baloo Bhaijaan', 'Bokor', 'Butterfly Kids', 'Calligraffitti', 'Caveat', 'Chilanka', 'Kristi', 'Lemonada', 'Londrina Shadow', 'Noto Sans SC', 'Shadows Into Light', 'Shadows Into Light Two', 'Snowburst One', 'Tajawal', 'Tangerine', 'ZCOOL KuaiLe', 'ZCOOL QingKe HuangYou'];
+const families = ['Tangerine', 'Akronim', 'Luckiest Guy', 'Lobster', 'Kanit', 'Wire One', 'Allura', 'Amatic SC', 'Baloo Bhaijaan', 'Bokor', 'Butterfly Kids', 'Calligraffitti', 'Caveat', 'Chilanka', 'Kristi', 'Lemonada', 'Londrina Shadow', 'Noto Sans SC', 'Shadows Into Light', 'Shadows Into Light Two', 'Snowburst One', 'Tajawal', 'Tangerine', 'ZCOOL KuaiLe', 'ZCOOL QingKe HuangYou'];
 
 WebFont.load({
     google: {
@@ -13,13 +13,15 @@ $(document).ready(function() {
 
     let view = 0;
     $("#preview").on("click", function(event) {
+        let text = $("#phrase").val();
         $("#previewArea").empty();
 
-        let text = $("#phrase").val();
         const previewDiv = `<div id="previewDiv" style="font-size: 36px">${text}</div>`
+        let fontFamily = $("#font").val();
+        let fontColor = $("#color").val();
         $("#previewArea").append(previewDiv);
-        $("#previewDiv").css("font-family", families[0]);
-        $("#previewDiv").css("color", "#e66465");
+        $("#previewDiv").css("font-family", fontFamily);
+        $("#previewDiv").css("color", fontColor);
 
 
 
@@ -50,8 +52,8 @@ $(document).ready(function() {
     $("#font").on("change", function() {
 
         let previewFont = ($("#font").val());
-
         $("#previewDiv").css("font-family", previewFont);
+        $("#font").css("font-family", previewFont);
 
     })
 
@@ -63,8 +65,36 @@ $(document).ready(function() {
 
         $("#previewDiv").css("color", fontColor);
 
+
     })
 
+    // Listen for Sign Up Click
 
+    $(document).on("click", "#submit", handleNewTattoo);
+
+
+    function handleNewTattoo(event) {
+        event.preventDefault();
+
+        let size = $("#size").val();
+        let font = $("#font").val();
+        let tattooSpec = $("#specify").val();
+        let bodyPart = $("#bodyPartDisplay").text();
+        let tattooText = $("#phrase").val();
+
+        sendTattoo({
+            text: tattooText,
+            font: font,
+            location: bodyPart,
+            colors: fontColor,
+            size: size,
+            specs: tattooSpec
+        });
+    }
+
+    function sendTattoo(data) {
+        $.post("api/Tattoos", data)
+        console.log(data);
+    }
 
 });
