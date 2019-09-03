a417cc784f680a6f30498e7f38942dc3c4695d95
Eric Billings <billings.company@gmail.com>
2019-09-03 09:26:49 -0400

    font change and color change funtionality working

--- public/js/fontAPI.js
+++ public/js/fontAPI.js
@@ -12,9 +12,14 @@ $(document).ready(function() {
     let view = 0;
     $("#preview").on("click", function(event) {
         $("#previewArea").empty();
+
         let text = $("#phrase").val();
         const previewDiv = `<div id="previewDiv" style="font-size: 36px">${text}</div>`
         $("#previewArea").append(previewDiv);
+        $("#previewDiv").css("font-family", families[0]);
+        $("#previewDiv").css("color", "#e66465");
+
+
 
         if (view === 0) {
             event.preventDefault();
@@ -33,21 +38,29 @@ $(document).ready(function() {
     for (let i = 0; i < families.length; i++) {
         const family = families[i].replace(/\s/g, "");
 
-        const option = `<option="${family} fontClass" id="dropDiv" value="${families[i]}" style="margin:5px; font-family:${families[i]}; font-size:18px" id=${family}>${families[i]}</option>`;
+        const option = `<option class="${family} fontClass" id="dropDiv" value="${families[i]}" style="margin:5px; font-family:${families[i]}; font-size:18px" id=${family}>${families[i]}</option>`;
         $("#font").append(option);
+        $("#font").css("font-family", families[0])
     }
 
-    $(".fontClass").on("mouseover", function() {
 
-        let fontFamily = $(this).css("font-family");
 
-        $("#previewDiv").css("font-family", fontFamily);
-    });
+    $("#font").on("change", function() {
 
-    $(".fontClass").on("click", function() {
-        $(".fontClass").hide();
+        let previewFont = ($("#font").val());
 
-    });
+        $("#previewDiv").css("font-family", previewFont);
+
+    })
+
+    $("#color").on("change", function() {
+
+        let fontColor = ($("#color").val());
+
+        $("#previewDiv").css("color", fontColor);
+
+    })
+
 
 
 });
