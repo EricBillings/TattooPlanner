369bdedae0225e3595e566120a26f0adfdb8cf1a
William White <willcwhite@gmail.com>
2019-09-04 10:56:36 -0400

    protects routes if not authenticated

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,7 +1,16 @@
 let path = require("path");
 
+
 module.exports = function (app) {
-  app.get("/details", function (req, res) {
+
+  function checkAuth (req,res,next){
+    if(req.isAuthenticated()){
+        return next()
+    }
+
+    res.redirect('/')
+}
+  app.get("/details",checkAuth, function (req, res) {
     res.sendFile(path.join(__dirname, "../public/details.html"));
   });
 
