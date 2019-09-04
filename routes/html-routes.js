c081035823a7ca8b1e59c9b6420c52a49dbac5fe
Lawrence Gober <goberlawrence@icloud.com>
2019-09-04 11:11:59 -0400

    Created signed in page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,6 +5,10 @@ module.exports = function (app) {
     res.sendFile(path.join(__dirname, "../public/details.html"));
   });
 
+  app.get("/signedin", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/signedin.html"));
+  });
+
   app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
