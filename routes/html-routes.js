ea050fd4758996b013d9496a68b5a7afc2753d58
William White <willcwhite@gmail.com>
2019-09-04 11:05:12 -0400

    checkAuth created

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,12 +9,20 @@ module.exports = function (app) {
     }
 
     res.redirect('/')
-}
+};
+
+  function checkNotAuth(req,res,next){
+    if(req.isAuthenticated()){
+      res.redirect('/details')
+    }
+    next()
+  }
+
   app.get("/details",checkAuth, function (req, res) {
     res.sendFile(path.join(__dirname, "../public/details.html"));
   });
 
-  app.get("/", function (req, res) {
+  app.get("/",checkNotAuth, function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 };
